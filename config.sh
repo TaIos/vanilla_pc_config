49fbad74d50da4523a0e0a88b15831e4b678b112
Martin Šafránek <martinsafranek1997@seznam.cz>
2018-08-24 16:35:23 +0200

    stage 0.1

--- config.sh
+++ config.sh
@@ -21,8 +21,8 @@ case "$1" in
 	apps|all)
 
 	APPS="vim git xclip"
-	PACMAN="dnf"
-	PACMAN_FLAGS="install -y"
+	PACMAN="pacman"
+	PACMAN_FLAGS="-Sy"
 
 	sudo $PACMAN $PACMAN_FLAGS $APPS
 	;;&
@@ -48,15 +48,63 @@ case "$1" in
 
 	GIT_PASSWORD=""
 	TMP_REPO='/tmp/git_repos'
+	HOME_DIR='/home/slarty'
 
 	read -sp 'GIT password: ' GIT_PASSWORD
-
+	
 	mkdir "${TMP_REPO}"
 	cd "${TMP_REPO}"
+
+	CLONED_DIRS="vimrc bashrc develop dokumenty"
 	git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/vimrc.git 
-	git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/bashrc.git
-	git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/develop.git
-	git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/dokumenty.git
+	VIMRC=vimrc
+	#git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/bashrc.git
+	#BASHRC=bashrc
+	#git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/develop.git
+	#DEVELOP=develop
+	#git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/dokumenty.git
+	#DOKUMENTY=dokumenty
+
+
+    setopt shwordsplit # splitting string as words
+
+	for dir in $CLONED_DIRS
+	do
+		if [ ! -d "${HOME_DIR}/${dir}" ]
+		then
+			#mkdir "${HOME_DIR}/${dir}"
+			echo "Directory $dir does not exits"
+		else
+			echo "Directory $dir exists"
+		fi
+	done
+
+	rm -rf "${TMP_REPO}"
+	exit 0
+
+
+	# VIMRC
+	if  [ -z "$(ls -A "${HOME_DIR}/${VIMRC}")" ]
+	then
+			echo "First branch"
+			cp -r "${TMP_REPO}/${VIMRC}/"* "${HOME_DIR}/${VIMRC}" 
+	else
+		echo "Second branch"
+		mkdir "${HOME_DIR}/${VIMRC}_tmp"
+		cp -r "${TMP_REPO}/${VIMRC}/"* "${HOME_DIR}/${VIMRC}_tmp"
+	fi
+
+	# BASHRC
+	#if  [ "$(ls -A "${HOME_DIR}/bashrc_dir")" ]
+	#then
+#
+	#fi
+#
+	## DEVELOP
+	#if  [ "$(ls -A "${HOME_DIR}/develop_dir")" ]
+#
+	## DOKUMENTY
+	#if  [ "$(ls -A "${HOME_DIR}/Dokumenty")" ]
 
 
 	rm -rf "${TMP_REPO}"
