5e4cb9b8137058d7bb939e313cd515b115c457b4
Martin Safranek <martinsafranek1997@seznam.cz>
2018-08-26 11:23:48 -0400

    alpha 1.2

--- config.sh
+++ config.sh
@@ -23,8 +23,8 @@ do
 		apps|all)
 
 		APPS="vim git xclip"
-		PACMAN="pacman"
-		PACMAN_FLAGS="-Sy"
+		PACMAN="dnf" # TODO make prompt
+		PACMAN_FLAGS="install -y"
 
 		sudo $PACMAN $PACMAN_FLAGS $APPS
 		;;&
@@ -48,11 +48,11 @@ do
 		# 4) Fetch GIT reposiroties & cpy them to home
 		git_fetch|all)
 
-		GIT_PASSWORD="1d0b23cb1666aa615728510ea2ff3005" // TODO CHANGE PASSWOD ON BITBUCKET !!!
+		GIT_PASSWORD="1d0b23cb1666aa615728510ea2ff3005" # TODO CHANGE PASSWOD ON BITBUCKET !!!
 		CLONED_DIRS="vimrc bashrc develop dokumenty"
-		CLONED_DIRS="vimrc bashrc develop" // TODO delete this line
+		CLONED_DIRS="vimrc bashrc develop" # TODO delete this line
 		TMP_DIR='/tmp/git_repos_tmp'
-		HOME_DIR='/home/slarty'
+		HOME_DIR='/home/liveuser' # TODO make prompt
 		TIMESTAMP=$(date +%Y-%m-%d.%H:%M:%S)
 
 		# create TMP_DIR if it doesn't exist
@@ -72,43 +72,35 @@ do
 		# (not splitting the string as individual words, interpreting as one string)
 		#setopt shwordsplit
 
-		# Loop throw all CLONED_DIR and ...
+		# Loop throw all CLONED_DIR ...
 		# a) Clone git repo to /tmp/TMP_DIR
-		# b) Create directory specified in CLONED_DIRS in HOME_DIR, if it dosn't exist
-		# c) Copy cloned git repos from /tmp/TMP_DIR to directories in HOME_DIR
+		# b) Backup overlapping directory in HOME_DIR
+		# c) Move cloned git repos from /tmp/TMP_DIR to HOME_DIR
 		for dir in $CLONED_DIRS
 		do
 			# a) Clone git repo
 			git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/"${dir}".git 
 
-			# b) Create directory in HOME_DIR if it doesn't exist
-			if [ ! -d "${HOME_DIR}/${dir}" ]
-			then
-				mkdir "${HOME_DIR}/${dir}"
-			fi
-
-			# c) Copy content from /tmp/TMP_DIR to directories in HOME_DIR
-			#
-			# if the target directory is not empty, backup it
-			if [ ! -z "$(ls -A  "${HOME_DIR}/${dir}")" ]
+			# b) If such directory exists in HOME_DIR, backup it
+			if [ -d "${HOME_DIR}/${dir}" ]
 			then
 				mv "${HOME_DIR}/${dir}" "${HOME_DIR}/${dir}_old_$TIMESTAMP"
 			fi
 
-			# copy the content
-			mv "${TMP_DIR}/${dir}/" "${HOME_DIR}/${dir}"
+			# c) move
+			mv "${TMP_DIR}/${dir}" "${HOME_DIR}"
 		done
 
 		#------------------------------
 		# Setup VIMRC
-		#
+		
 		# backup old vimrc, if there is any
 		if [ -e "${HOME_DIR}/.vimrc" ]
 		then
 			mv "${HOME_DIR}/.vimrc" "${HOME_DIR}/.vimrc_old_$TIMESTAMP"
 		fi
 
-		# backup .vimrc_dir, if there is any
+		# backup old .vimrc_dir, if there is any
 		if [ -e "${HOME_DIR}/.vimrc_dir" ]
 		then
 			mv "${HOME_DIR}/.vimrc_dir" "${HOME_DIR}/.vimrc_dir_old_$TIMESTAMP"
@@ -130,7 +122,7 @@ do
 			mv "${HOME_DIR}/.bashrc" "${HOME_DIR}/.bashrc_old_$TIMESTAMP"
 		fi
 
-		# backup .bashrc_dir, if there is any
+		# backup old .bashrc_dir, if there is any
 		if [ -e "${HOME_DIR}/.bashrc_dir" ]
 		then
 			mv "${HOME_DIR}/.bashrc_dir" "${HOME_DIR}/.bashrc_dir_old_$TIMESTAMP"
@@ -140,7 +132,7 @@ do
 		mv "${HOME_DIR}/bashrc" "${HOME_DIR}/.bashrc_dir"
 
 		# set symlink to correct bashrc (bashrc_fedora, bashrc_arch, bashrc_ubuntu)
-		$SYSTEM_ID=$(cat /etc/*-release | grep 'ID=' |cut  -d'=' -f2)
+		SYSTEM_ID=$(cat /etc/*-release | grep 'ID=' |cut  -d'=' -f2)
 		ln -sf "${HOME_DIR}/.bashrc_dir/bashrc_$SYSTEM_ID" "${HOME_DIR}/.bashrc" 
 
 		source "${HOME_DIR}/.bashrc"
