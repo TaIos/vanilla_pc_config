950a0ee16e6b6b817b7ae2b627ae628ff0a2724e
Martin Šafránek <martinsafranek1997@seznam.cz>
2018-08-26 11:54:44 +0200

    stage 0.2

--- config.sh
+++ config.sh
@@ -4,111 +4,111 @@
 ## after installing new Linux system
 #
 #
-## Particular sections can be invoked passing argument
+## Particular sections can be invoked passing argument/s
 ## arguments: all, terminal, apps, git_config, git_fetch
 
-case "$1" in
+for arg in "$@"
+do
+	case "$arg" in
 
-	#-------------------------------------------------------------------------------
-	# 1) Set gnome-terminal to open mazimized
-	terminal|all)
+		#-------------------------------------------------------------------------------
+		# 1) Set gnome-terminal to open mazimized
+		terminal|all)
 
-	sudo sed -i 's/^Exec=gnome-terminal$/Exec=gnome-terminal --window --maximize/' /usr/share/applications/org.gnome.Terminal.desktop
-	;;&
-	
-	#-------------------------------------------------------------------------------
-	# 2) Install necessary applications
-	apps|all)
+		sudo sed -i 's/^Exec=gnome-terminal$/Exec=gnome-terminal --window --maximize/' /usr/share/applications/org.gnome.Terminal.desktop
+		;;&
+		
+		#-------------------------------------------------------------------------------
+		# 2) Install necessary applications
+		apps|all)
 
-	APPS="vim git xclip"
-	PACMAN="pacman"
-	PACMAN_FLAGS="-Sy"
+		APPS="vim git xclip"
+		PACMAN="pacman"
+		PACMAN_FLAGS="-Sy"
 
-	sudo $PACMAN $PACMAN_FLAGS $APPS
-	;;&
+		sudo $PACMAN $PACMAN_FLAGS $APPS
+		;;&
 
-	#-------------------------------------------------------------------------------
-	# 3) Setup GIT
-	git_config|all)
+		#-------------------------------------------------------------------------------
+		# 3) Setup GIT
+		git_config|all)
 
-	git config --global user.name "Martin Safranek"
-	git config --global user.email "martinsafranek1997@seznam.cz"
-	git config --global core.editor vim
+		git config --global user.name "Martin Safranek"
+		git config --global user.email "martinsafranek1997@seznam.cz"
+		git config --global core.editor vim
 
-	git config --global alias.co checkout
-	git config --global alias.br branch
-	git config --global alias.ci commit
-	git config --global alias.st status
-	git config --global alias.ll 'log --oneline --graph --all --decorate'
-	;;&
+		git config --global alias.co checkout
+		git config --global alias.br branch
+		git config --global alias.ci commit
+		git config --global alias.st status
+		git config --global alias.ll 'log --oneline --graph --all --decorate'
+		;;&
 
-	#-------------------------------------------------------------------------------
-	# 4) Fetch GIT reposiroties
-	git_fetch|all)
+		#-------------------------------------------------------------------------------
+		# 4) Fetch GIT reposiroties & cpy them to home
+		git_fetch|all)
 
-	GIT_PASSWORD=""
-	TMP_REPO='/tmp/git_repos'
-	HOME_DIR='/home/slarty'
+		GIT_PASSWORD="test"
+		CLONED_DIRS="vimrc bashrc develop dokumenty"
+		CLONED_DIRS="vimrc bashrc develop"
+		TMP_REPO='/tmp/git_repos_tmp'
+		HOME_DIR='/home/slarty'
 
-	read -sp 'GIT password: ' GIT_PASSWORD
-	
-	mkdir "${TMP_REPO}"
-	cd "${TMP_REPO}"
-
-	CLONED_DIRS="vimrc bashrc develop dokumenty"
-	git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/vimrc.git 
-	VIMRC=vimrc
-	#git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/bashrc.git
-	#BASHRC=bashrc
-	#git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/develop.git
-	#DEVELOP=develop
-	#git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/dokumenty.git
-	#DOKUMENTY=dokumenty
-
-
-    setopt shwordsplit # splitting string as words
-
-	for dir in $CLONED_DIRS
-	do
-		if [ ! -d "${HOME_DIR}/${dir}" ]
+		# create TMP_REPO if it doesn't exist
+		if [ ! -d "${TMP_REPO}" ]
 		then
-			#mkdir "${HOME_DIR}/${dir}"
-			echo "Directory $dir does not exits"
-		else
-			echo "Directory $dir exists"
+			mkdir "${TMP_REPO}"
 		fi
-	done
 
-	rm -rf "${TMP_REPO}"
-	exit 0
+		cd "${TMP_REPO}"
 
 
-	# VIMRC
-	if  [ -z "$(ls -A "${HOME_DIR}/${VIMRC}")" ]
-	then
-			echo "First branch"
-			cp -r "${TMP_REPO}/${VIMRC}/"* "${HOME_DIR}/${VIMRC}" 
-	else
-		echo "Second branch"
-		mkdir "${HOME_DIR}/${VIMRC}_tmp"
-		cp -r "${TMP_REPO}/${VIMRC}/"* "${HOME_DIR}/${VIMRC}_tmp"
-	fi
+		if [ -z "$GIT_PASSWORD" ]
+		then
+			read -sp 'GIT password: ' GIT_PASSWORD
+		fi
 
-	# BASHRC
-	#if  [ "$(ls -A "${HOME_DIR}/bashrc_dir")" ]
-	#then
-#
-	#fi
-#
-	## DEVELOP
-	#if  [ "$(ls -A "${HOME_DIR}/develop_dir")" ]
-#
-	## DOKUMENTY
-	#if  [ "$(ls -A "${HOME_DIR}/Dokumenty")" ]
+		# Enable if shell is not interpreting string as words 
+		# (not splitting the string as individual words, interpreting as one string)
+		#setopt shwordsplit
 
+		# 1) Clone git repos to /tmp/TMP_REPO
+		# 2) Create dirs specified in CLONED_DIRS in HOME_DIR, if they do not exist
+		# 3) Copy cloned git repos from /tmp/TMP_REPO to created dirs in HOME_DIR
+		# 4) Setup vimrc/bashrc
+		for dir in $CLONED_DIRS
+		do
 
-	rm -rf "${TMP_REPO}"
-	;;&
+			echo "$dir"
+			continue
+			# 1) clone git repos
+			git clone https://kaldomain:"${GIT_PASSWORD}"@bitbucket.org/kaldomain/"${dir}".git 
 
-	#-------------------------------------------------------------------------------
-esac
+			# 2) create dirs
+			if [ ! -d "${HOME_DIR}/${dir}" ]
+			then
+				mkdir "${HOME_DIR}/${dir}"
+			fi
+
+			# 3) copy to dirs
+			# if the directory is empty, copy to it
+			if [ -z "$(ls -A  "${HOME_DIR}/${dir}")" ]
+			then
+					cp -r "${TMP_DIR}/$dir}" "${HOME_DIR}/${dir}"
+			# directory is not empty, create HOME_DIR/dir tmp dir and copy to it
+			else
+				mkdir "${HOME_DIR}/${dir}_tmp"
+				cp -r "${TMP_DIR}/$dir}" "${HOME_DIR}/${dir}_tmp"
+			fi
+
+			# 4)
+
+		done
+
+		# cleanup /tmp/TMP_REPO
+		rm -rf "${TMP_REPO}"
+		;;&
+		
+		#-------------------------------------------------------------------------------
+	esac
+done
